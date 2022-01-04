b156168cb02bcdb4749f96e73010d46d26fb1a72
Dan Fey <feydanm@gmail.com>
2022-01-03 19:26:07 -0800

    removing sudo

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # install steamcmd: https://developer.valvesoftware.com/wiki/SteamCMD?__cf_chl_jschl_tk__=pmd_WNQPOiK18.h0rf16RCYrARI2s8_84hUMwT.7N1xHYcs-1635248050-0-gqNtZGzNAiWjcnBszQiR#Linux.2FmacOS)
-sudo useradd -m steam
+useradd -m steam
 
-sudo add-apt-repository multiverse
-sudo dpkg --add-architecture i386
-sudo apt update
+add-apt-repository multiverse
+dpkg --add-architecture i386
+apt update
 
-sudo apt install lib32gcc1 steamcmd
+apt install lib32gcc1 steamcmd
 
 # install satisfactory: https://satisfactory.fandom.com/wiki/Dedicated_servers
 steamcmd +login anonymous +app_update 1690800 -beta experimental validate +quit
@@ -62,7 +62,7 @@ while [ $isIdle -le 0 ]; do
 done
 
 echo "No activity detected for $shutdownIdleMinutes minutes, shutting down."
-sudo shutdown -h now
+shutdown -h now
 EOF
 chmod +x /home/ubuntu/auto-shutdown.sh
 chown ubuntu:ubuntu /home/ubuntu/auto-shutdown.sh
