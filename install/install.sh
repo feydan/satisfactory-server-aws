5b4a0d039309dfe246149be0fbd460dfacdeae13
Dan Fey <feydanm@gmail.com>
2022-01-03 19:23:51 -0800

    first pass at install script

--- install/install.sh
+++ install/install.sh
@@ -7,11 +7,83 @@ sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
 
-
 sudo apt install lib32gcc1 steamcmd
 
 # install satisfactory: https://satisfactory.fandom.com/wiki/Dedicated_servers
 steamcmd +login anonymous +app_update 1690800 -beta experimental validate +quit
 
-# enable as server so it stays up: https://satisfactory.fandom.com/wiki/Dedicated_servers/Running_as_a_Service
+# enable as server so it stays up and start: https://satisfactory.fandom.com/wiki/Dedicated_servers/Running_as_a_Service
+cat << EOF > /etc/systemd/system/satisfactory.service
+[Unit]
+Description=Satisfactory dedicated server
+Wants=network-online.target
+After=syslog.target network.target nss-lookup.target network-online.target
 
+[Service]
+Environment="LD_LIBRARY_PATH=./linux64"
+ExecStartPre=/usr/games/steamcmd +login anonymous +app_update 1690800 -beta experimental validate +quit
+ExecStart=/home/ubuntu/.steam/steamapps/common/SatisfactoryDedicatedServer/FactoryServer.sh
+User=ubuntu
+Group=ubuntu
+StandardOutput=journal
+Restart=on-failure
+KillSignal=SIGINT
+WorkingDirectory=/home/ubuntu/.steam/steamapps/common/SatisfactoryDedicatedServer
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable satisfactory
+systemctl start satisfactory
+
+# enable auto shutdown: https://github.com/feydan/satisfactory-tools/tree/main/shutdown
+cat << EOF > /home/ubuntu/auto-shutdown.sh
+#!/bin/sh
+
+shutdownIdleMinutes=30
+idleCheckFrequencySeconds=1
+
+isIdle=0
+while [ $isIdle -le 0 ]; do
+    isIdle=1
+    iterations=$((60 / $idleCheckFrequencySeconds * $shutdownIdleMinutes))
+    while [ $iterations -gt 0 ]; do
+        sleep $idleCheckFrequencySeconds
+        connectionBytes=$(ss -lu | grep 777 | awk -F ' ' '{s+=$2} END {print s}')
+        if [ ! -z $connectionBytes ] && [ $connectionBytes -gt 0 ]; then
+            isIdle=0
+        fi
+        if [ $isIdle -le 0 ] && [ $(($iterations % 21)) -eq 0 ]; then
+           echo "Activity detected, resetting shutdown timer to $shutdownIdleMinutes minutes."
+           break
+        fi
+        iterations=$(($iterations-1))
+    done
+done
+
+echo "No activity detected for $shutdownIdleMinutes minutes, shutting down."
+sudo shutdown -h now
+EOF
+chmod +x /home/ubuntu/auto-shutdown.sh
+chown ubuntu:ubuntu /home/ubuntu/auto-shutdown.sh
+
+cat << EOF > /etc/systemd/system/auto-shutdown.service
+[Unit]
+Description=Auto shutdown if no one is playing Satisfactory
+After=syslog.target network.target nss-lookup.target network-online.target
+
+[Service]
+Environment="LD_LIBRARY_PATH=./linux64"
+ExecStart=/home/ubuntu/auto-shutdown.sh
+User=ubuntu
+Group=ubuntu
+StandardOutput=journal
+Restart=on-failure
+KillSignal=SIGINT
+WorkingDirectory=/home/ubuntu
+EOF
+systemctl enable auto-shutdown
+systemctl start auto-shutdown
+
+# automated backups to s3 every 5 minutes (current disabled because s3 bucket needs to be passed in)
+# crontab -l -e ubuntu | { cat; */5 * * * * aws s3 sync --acl public-read /home/ubuntu/.config/Epic/FactoryGame/Saved/SaveGames/server <s3 bucket>; } | crontab -
