282150ddac5314d2581951065da94b22f02db780
Dan Fey <feydanm@gmail.com>
2022-01-06 15:59:47 -0800

    uncommenting lines in config.ts

--- server-hosting/config.sample.ts
+++ server-hosting/config.sample.ts
@@ -1,33 +1,33 @@
-// export const Config = {
-//     // compulsory parameters
-//
-//     // server hosting region
-//     region: '',
-//     // server hosting account
-//     account: '',
-//     // prefix for all resources in this app
-//     prefix: 'SatisfactoryHosting',
-//     // set to false if you don't want an api to
-//     // restart game server and true if you do
-//     restartApi: true,
-//     // Set to true if you want to use Satisfactory Experimental
-//     useExperimentalBuild: false,
-//
-//     // optional parameters
-//
-//     // bucket for storing save files
-//     // you can use an existing bucket
-//     // or leave it empty to create a new one
-//     bucketName: 'satisfactory-something',
-//     // server hosting vpc
-//     // Create a vpc and it's id here
-//     // or leave it empty to use default vpc
-//     vpcId: '',
-//     // specify server subnet
-//     // leave blank (preferred option) for auto-placement
-//     // If vpc is given specify subnet for that vpc
-//     // If vpc is not given specify subnet for default vpc
-//     subnetId: '',
-//     // Needed if subnetId is specified (i.e. us-west-2a)
-//     availabilityZone: ''
-// };
+export const Config = {
+     // compulsory parameters
+
+     // server hosting region
+     region: '',
+     // server hosting account
+     account: '',
+     // prefix for all resources in this app
+     prefix: 'SatisfactoryHosting',
+     // set to false if you don't want an api to
+     // restart game server and true if you do
+     restartApi: true,
+     // Set to true if you want to use Satisfactory Experimental
+     useExperimentalBuild: false,
+
+     // optional parameters
+
+     // bucket for storing save files
+     // you can use an existing bucket
+     // or leave it empty to create a new one
+     bucketName: '',
+     // server hosting vpc
+     // Create a vpc and it's id here
+     // or leave it empty to use default vpc
+     vpcId: '',
+     // specify server subnet
+     // leave blank (preferred option) for auto-placement
+     // If vpc is given specify subnet for that vpc
+     // If vpc is not given specify subnet for default vpc
+     subnetId: '',
+     // Needed if subnetId is specified (i.e. us-west-2a)
+     availabilityZone: ''
+};
